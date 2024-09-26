5e0f812acdb72b343c6b089faa0c4644a6062f78
hgarrereyn <harrisonmichaelgreen@gmail.com>
2024-09-25 20:56:30 -0400

    fix for ida7.5

--- fuzzer-code/BB-weight4-py3.py
+++ fuzzer-code/BB-weight4-py3.py
@@ -10,9 +10,11 @@
 import idaapi
 import idautils
 import idc
-#from idaapi import *
-#from idautils import *
-#from idc import *
+import ida_bytes
+import ida_nalt
+# from idaapi import *
+# from idautils import *
+# from idc import *
 from collections import deque
 #import json
 import timeit
@@ -33,15 +35,15 @@ def findCMPopnds():
     result1=set()#contains full strings as they appear in the instruction 
     result2=set()#contains bytes of strings in instruction
     # For each of the segments
-    for seg_ea in Segments():
-        for head in Heads(seg_ea, SegEnd(seg_ea)):
-            if isCode(GetFlags(head)):
-                mnem = GetMnem(head)
+    for seg in idautils.Segments():
+        for head in idautils.Heads(idc.get_segm_start(seg), idc.get_segm_end(seg)):
+            if ida_bytes.is_code(idc.get_full_flags(head)):
+                mnem = idc.print_insn_mnem(head)
                 #print mnem
                 if mnem in cmpl:
                     for i in range(2):
-                        if GetOpType(head,i)==5:
-                            names.add(GetOpnd(head,i))
+                        if idc.get_operand_type(head,i)==5:
+                            names.add(idc.print_operand(head,i))
                         
     for el in names:
         tm=el.rstrip('h')
@@ -50,7 +52,7 @@ def findCMPopnds():
         if len(tm)%2 !=0:
             tm='0'+tm
         whx=''
-        for i in xrange(0,len(tm),2):
+        for i in range(0,len(tm),2):
             #hx=chr('\\x'+tm[i:i+2]
             hx=chr(int(tm[i:i+2],16))
             result2.add(hx)
@@ -66,27 +68,27 @@ def get_children(BB):
     '''
     This function returns a list of BB ids which are children (transitive) of given BB.
     '''
-    print("[*] finding childrens of BB: %x"%(BB.startEA,))
+    print("[*] finding childrens of BB: %x"%(BB.start_ea,))
     child=[]
     tmp=deque([])
     tmpShadow=deque([])
     #visited=[]
     for sbb in BB.succs():
         tmp.append(sbb)
-        tmpShadow.append(sbb.startEA)
+        tmpShadow.append(sbb.start_ea)
     if len(tmp) == 0:
         return child
     while len(tmp)>0:
         cur=tmp.popleft()
         tmpShadow.popleft()
-        if cur.startEA not in child:
+        if cur.start_ea not in child:
             
-            child.append(cur.startEA)
+            child.append(cur.start_ea)
         for cbbs in cur.succs():
-            if (cbbs.startEA not in child) and  (cbbs.startEA not in tmpShadow):
+            if (cbbs.start_ea not in child) and  (cbbs.start_ea not in tmpShadow):
                 
                 tmp.append(cbbs)    
-                tmpShadow.append(cbbs.startEA)
+                tmpShadow.append(cbbs.start_ea)
     del tmp
     del tmpShadow
     return child
@@ -113,12 +115,12 @@ def calculate_weight(func, fAddr):
         eProb=1.0/pLen #probability of each outgoing edge
         #print "probability = %3.1f"%(eProb,), eProb
         for succBB in block.succs():
-            if (succBB.startEA <= block.startEA) and (len(list(succBB.preds()))>1):
+            if (succBB.start_ea <= block.start_ea) and (len(list(succBB.preds()))>1):
                 #this is for backedge. this is not entirely correct as BB which are shared or are at lower
                 #addresses are tagged as having zero value!! TO FIX.
-                edges[(block.startEA,succBB.startEA)]=1.0
+                edges[(block.start_ea,succBB.start_ea)]=1.0
             else:
-                edges[(block.startEA,succBB.startEA)]=eProb
+                edges[(block.start_ea,succBB.start_ea)]=eProb
     print("[*] Finished edge probability calculation")
     #for edg in edges:
         #print " %x -> %x: %3.1f "%(edg[0],edg[1],edges[edg])
@@ -129,7 +131,7 @@ def calculate_weight(func, fAddr):
         if len(list(block.preds())) == 0:
         #Note: this only check was not working as there are orphan BB in code. Really!!!
 
-            if block.startEA == fAddr:
+            if block.start_ea == fAddr:
                 rootFound=True
                 root = block
             else:
@@ -142,33 +144,33 @@ def calculate_weight(func, fAddr):
         rch=get_children(root)
         rch.append(fAddr)# add root also as a non-orphan BB
         for blk in func:
-            if blk.startEA not in rch:
-                weight[blk.startEA]=(1.0,blk.endEA)
+            if blk.start_ea not in rch:
+                weight[blk.start_ea]=(1.0,blk.end_ea)
                 visited.append(blk.id)
                 orphID.append(blk.id)
         #print "[*] orphanage calculation done."
         del rch
     if rootFound==True:
-        #print "[*] found root BB at %x"%(root.startEA,)
-        weight[root.startEA] = (1.0,root.endEA)
+        #print "[*] found root BB at %x"%(root.start_ea,)
+        weight[root.start_ea] = (1.0,root.end_ea)
         visited.append(root.id)
         print("[*] Root found. Starting weight calculation.")
         for sBlock in root.succs():
             #if sBlock.id not in shadow:
-            #print "Pushing successor %x"%(sBlock.startEA,)
+            #print "Pushing successor %x"%(sBlock.start_ea,)
             temp.append(sBlock)
             shadow.append(sBlock.id)
         loop=dict()# this is a temp dictionary to avoid get_children() call everytime a BB is analysed.
         while len(temp) > 0:
             current=temp.popleft()
             shadow.remove(current.id)
-            print("current: %x"%(current.startEA,))
+            print("current: %x"%(current.start_ea,))
             if current.id not in loop:
                 loop[current.id]=[]
             # we check for orphan BB and give them a lower score
             # by construction and assumptions, this case should not hit!
             if current.id in orphID:
-                #weight[current.startEA]=(0.5,current.endEA)
+                #weight[current.start_ea]=(0.5,current.end_ea)
                 #visited.append(current.id)
                 continue
 
@@ -176,10 +178,10 @@ def calculate_weight(func, fAddr):
             stillNot=False
             chCalculated=False
             for pb in current.preds():
-                #print "[*] pred of current %x"%(pb.startEA,)
+                #print "[*] pred of current %x"%(pb.start_ea,)
                 if pb.id not in visited:
-                    if edges[(pb.startEA,current.startEA)]==0.0:
-                        weight[pb.startEA]=(0.5,pb.endEA)
+                    if edges[(pb.start_ea,current.start_ea)]==0.0:
+                        weight[pb.start_ea]=(0.5,pb.end_ea)
                         #artificial insertion
                         #print "artificial insertion branch"
                         continue
@@ -187,9 +189,9 @@ def calculate_weight(func, fAddr):
                         if chCalculated == False:
                             chCurrent=get_children(current)
                             chCalculated=True
-                        if pb.startEA in chCurrent:
+                        if pb.start_ea in chCurrent:
                             # this BB is in a loop. we give less score to such BB
-                            weight[pb.startEA]=(0.5,pb.endEA)
+                            weight[pb.start_ea]=(0.5,pb.end_ea)
                             loop[current.id].append((pb.id,True))
                             #print "loop branch"
                             continue
@@ -197,13 +199,13 @@ def calculate_weight(func, fAddr):
                             loop[current.id].append((pb.id,False))
                     else:
                         if (pb.id,True) in loop[current.id]:
-                            weight[pb.startEA]=(0.5,pb.endEA)
+                            weight[pb.start_ea]=(0.5,pb.end_ea)
                             continue
                             
-                    #print "not pred %x"%(pb.startEA,)
+                    #print "not pred %x"%(pb.start_ea,)
                     if current.id not in shadow:
                         temp.append(current)
-                        #print "pushed back %x"%(current.startEA,)
+                        #print "pushed back %x"%(current.start_ea,)
                         shadow.append(current.id)
                     stillNot=True
                     break
@@ -216,25 +218,25 @@ def calculate_weight(func, fAddr):
                         temp.append(sb)
                         shadow.append(sb.id)
                 for pb in current.preds():
-                    tempSum = tempSum+ (weight[pb.startEA][0]*edges[(pb.startEA,current.startEA)])
-                weight[current.startEA] = (tempSum,current.endEA)
+                    tempSum = tempSum+ (weight[pb.start_ea][0]*edges[(pb.start_ea,current.start_ea)])
+                weight[current.start_ea] = (tempSum,current.end_ea)
                 visited.append(current.id)
                 del loop[current.id]
-                print("completed %x"%(current.startEA,))
+                print("completed %x"%(current.start_ea,))
                 #print "remaining..."
                 #for bs in temp:
-                    #print "\t %x"%(bs.startEA,)
+                    #print "\t %x"%(bs.start_ea,)
 
 def analysis():
     global fCount
     all_funcs = idautils.Functions()
     for f in all_funcs:
-        fflags=idc.GetFunctionFlags(f)
-        if (fflags & FUNC_LIB) or (fflags & FUNC_THUNK):
+        fflags=idc.get_func_attr(f, idc.FUNCATTR_FLAGS)
+        if (fflags & idc.FUNC_LIB) or (fflags & idc.FUNC_THUNK):
             continue
         fCount = fCount+1
-        print("In %s:\n"%(idc.GetFunctionName(f),))
-        fAddr=GetFunctionAttr(f,FUNCATTR_START)
+        print("In %s:\n"%(idc.get_func_name(f),))
+        fAddr=idc.get_func_attr(f,idc.FUNCATTR_START)
         f = idaapi.FlowChart(idaapi.get_func(f),flags=idaapi.FC_PREDS)
         calculate_weight(f,fAddr)
         
@@ -256,16 +258,18 @@ def main():
     print(" [**] Total Time: ", stop - start)
     print("[**] Total functions analyzed: %d"%(fCount,))
     print("[**] Total BB analyzed: %d"%(len(fweight),))
-    outFile=GetInputFile() # name of the that is being analysed
+    outFile=ida_nalt.get_root_filename() # name of the that is being analysed
     strFile=outFile+".names"
     outFile=outFile+".pkl"
-    fd=open(outFile,'w')
+    fd=open(outFile,'wb')
     pickle.dump(fweight,fd)
     fd.close()
-    strFD=open(strFile,'w')
+    strFD=open(strFile,'wb')
     pickle.dump(strings,strFD)
     strFD.close()
     print("[*] Saved results in pickle files: %s, %s"%(outFile,strFile))
 
 if __name__ == "__main__":
     main()
+
+main()
